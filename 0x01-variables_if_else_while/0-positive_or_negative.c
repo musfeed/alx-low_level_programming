c4f34069873ca0425c9d3020acc0e98e9f9a973b
Mustapha <tplmustyabu@gmail.com>
2022-06-03 13:22:25 +0100

    added missing newline

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Return: Always 0
+ **/
 int main(void)
 {
 	int n;
@@ -13,13 +15,13 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 	{
-		printf("%d is positive",n);
+		printf("%d is positive\n",n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative",n);
+		printf("%d is negative\n",n);
 	}
 	else
-		printf("%d is zero",n);
+		printf("%d is zero\n",n);
 	return (0);
 }
