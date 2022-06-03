a262e3aaa2fcc1bbd3abafa1f39dcfba08a3e7d6
Mustapha <tplmustyabu@gmail.com>
2022-06-03 14:34:38 +0100

    print alphabet with putchar finished

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ **/
+int main(void)
+{
+	int i;
+
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
