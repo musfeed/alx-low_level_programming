e76eeea1ea715d27abb3f249686c96cb3b7fbbe7
Mustapha <tplmustyabu@gmail.com>
2022-06-19 13:23:27 +0100

    task 6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers: print 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(10);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - prints _
+ * @n: number of _ to print
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for(i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar(10);
+}
