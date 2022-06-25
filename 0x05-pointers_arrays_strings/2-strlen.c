60ca5c52757874f948db7be5a770a8309b7b059b
Mustapha <tplmustyabu@gmail.com>
2022-06-25 23:09:13 +0100

    task 2 pointer task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,14 @@
 /**
  * _strlen - function that returns the length of a string
  * @s: the string to coutn
+ * Return: return the length of string
  **/
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
