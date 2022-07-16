befb02916b7d3a66148aa060134a3eff56250000
Mustapha <tplmustyabu@gmail.com>
2022-07-16 17:44:10 +0100

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,20 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-			printf(" Fizz");
-		else if (i % 5 == 0 && i % 3 != 0)
-			printf(" Buzz");
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf(" FIzzBuzz");
+		if (i > 1)
+		{
+			putchar(32);
+			if (i % 3 == 0 && i % 5 != 0)
+				printf("Fizz");
+			else if (i % 5 == 0 && i % 3 != 0)
+				printf("Buzz");
+			else if ((i % 3 == 0) && (i % 5 == 0))
+				printf("FIzzBuzz");
+			else
+				printf("%d", i);
+		}
 		else if (i == 1)
 			printf("%d", i);
-		else
-			printf(" %d", i);
 	}
 	return (0);
 }
